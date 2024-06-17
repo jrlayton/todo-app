4ebb85e2a4a0d3f68b28f8fd152a9c6ff64392b4
jrlayton <jreaylayton@gmail.com>
2024-06-16 21:01:02 -0700

    Update styles for completed and hover states, add props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import Icon from "./Icon";
 import Card from "./Card";
 
-function Todo({ description, completed, onClick, className }) {
-  const [isCompleted, setIsCompleted] = useState(completed);
-
-  const markCompleted = () => {};
-
+function Todo({ description, completed, setCompleted, onClick, className }) {
   return (
     <Card className={className}>
       <div className="flex flex-row items-center w-full h-full">
-        <div className="flex justify-center items-center rounded-full border-solid border-light-very-light-gray dark:border-dark-very-dark-grayish-blue-1 border-2 h-6 w-6 mr-6">
-          <Icon
-            name="check"
-            onClick={markCompleted}
-            className={isCompleted ? "visible" : "invisible"}
-          />
+        <div className="rounded-full bg-light-very-light-grayish-blue dark:bg-dark-very-dark-grayish-blue-1 h-6 w-6 mr-6 hover:cursor-pointer hover:bg-check-background p-[1px]">
+          <div
+            className={`flex items-center justify-center w-full h-full rounded-full bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue ${
+              completed ? "bg-check-background" : ""
+            }`}
+            onClick={setCompleted}
+          >
+            <Icon
+              name="check"
+              className={`${completed ? "visible" : "invisible"}`}
+            />
+          </div>
         </div>
-        <span className="flex-1 text-light-very-light-gray dark:text-dark-light-grayish-blue">
+        <span
+          className={`flex-1 text-light-very-dark-grayish-blue dark:text-dark-light-grayish-blue ${
+            completed
+              ? "line-through text-light-l-g-b dark:text-dark-very-dark-grayish-blue-1"
+              : ""
+          }`}
+        >
           {description}
         </span>
         <Icon
