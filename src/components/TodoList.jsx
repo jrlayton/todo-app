a3f688732538dd8e2105f01b377df437f59ca4f0
jrlayton <jreaylayton@gmail.com>
2024-07-22 21:28:40 -0700

    Refactor file name, function name, and default export
    Implements layout change from large to md to small screen sizes. The list filter widget (All, Active, Completed) will appear below the list in it's own "card" on smaller screen sizes.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,66 +1,152 @@
 import React, { useState } from "react";
-import Todo from "./Todo";
+import ThemeToggle from "./ThemeToggle";
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
 import Card from "./Card";
 
-function TodoList({ todos, setTodos }) {
+function TodoList({ className }) {
+  const [list, setList] = useState([
+    {
+      id: 0,
+      completed: false,
+      description: "10 minutes meditation",
+    },
+    {
+      id: 1,
+      completed: false,
+      description: "Jog around the park 3x",
+    },
+    {
+      id: 2,
+      completed: false,
+      description: "Read for 1 hour",
+    },
+    {
+      id: 3,
+      completed: false,
+      description: "Pick up groceries",
+    },
+    {
+      id: 4,
+      completed: false,
+      description: "Complete Todo App",
+    },
+  ]);
+
   const [filter, setFilter] = useState({
     all: false,
     active: false,
     completed: false,
   });
 
-  const markCompleted = (id) => {
-    setTodos((prev) => {
-      return prev.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+  const filteredList = list.filter((item) => {
+    if (filter.all) return true;
+    if (filter.active) return !item.completed;
+    if (filter.completed) return item.completed;
+    return true;
+  });
+
+  function markItemCompleted(id) {
+    setList((prev) => {
+      return prev.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
       );
     });
-  };
+  }
 
-  const clearTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  function removeItem(id) {
+    setList(list.filter((item) => item.id !== id));
+  }
 
-  const handleFilter = (event) => {
+  function handleFilter(event) {
     const selectedFilter = event.target.innerText.toLowerCase();
     setFilter({
       all: selectedFilter === "all",
       active: selectedFilter === "active",
       completed: selectedFilter === "completed",
     });
-  };
+  }
 
-  const clearCompleted = () => {
-    setTodos(todos.filter((todo) => !todo.completed));
-  };
-
-  const filteredTodos = todos.filter((todo) => {
-    if (filter.all) return true;
-    if (filter.active) return !todo.completed;
-    if (filter.completed) return todo.completed;
-    return true;
-  });
+  function clearCompleted() {
+    setList(list.filter((item) => !item.completed));
+  }
 
   return (
-    <div className="divide-y divide-light-light-grayish-blue dark:divide-dark-very-dark-grayish-blue-2 rounded-md shadow-2xl shadow-light-light-grayish-blue dark:shadow-dark-very-dark-blue">
-      {filteredTodos.map((todo, index) => (
-        <Todo
-          key={todo.id}
-          description={todo.description}
-          completed={todo.completed}
-          setCompleted={() => markCompleted(todo.id)}
-          onClick={() => clearTodo(todo.id)}
-          className={`${index === 0 ? "rounded-t-md" : ""}`}
-        />
-      ))}
-      <Card className={`${todos.length === 0 ? "rounded-md" : "rounded-b-md"}`}>
-        <div className="flex flex-row items-center rounded-b-md">
-          <span className="text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue">
-            {`${
-              filteredTodos.filter((todo) => !todo.completed).length
-            } items left`}
-          </span>
-          <div className="flex flex-row gap-4 mx-auto group text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue">
+    <div
+      className={`${className} relative h-[100svh] w-[95vw] min-w-[320px] sm:max-w-[600px] mx-auto font-josefin`}
+    >
+      <div className="flex flex-col gap-x-[1vw] gap-y-[3vh] py-12 px-4 sm:gap-8 sm:py-20 sm:px-5">
+        <div className="flex flex-row items-center justify-between">
+          <h1 className="mt-2 text-2xl font-bold tracking-widest select-none sm:text-4xl text-light-very-light-gray">
+            TODO
+          </h1>
+          <ThemeToggle />
+        </div>
+        <TodoForm list={list} setList={setList} />
+        <div className="divide-y rounded-md shadow-2xl divide-light-light-grayish-blue dark:divide-dark-very-dark-grayish-blue-2 shadow-light-light-grayish-blue dark:shadow-dark-very-dark-blue">
+          {filteredList.map((item, index) => (
+            <div key={item.id}>
+              <TodoItem
+                description={item.description}
+                completed={item.completed}
+                setCompleted={() => markItemCompleted(item.id)}
+                onClick={() => removeItem(item.id)}
+                className={`${index === 0 ? "rounded-t-md" : ""}`}
+              />
+            </div>
+          ))}
+          <Card
+            className={`${
+              filteredList.length === 0 ? "rounded-md" : "rounded-b-md"
+            } py-4`}
+          >
+            <div className="flex flex-row items-center justify-between select-none rounded-b-md">
+              <span className="text-sm sm:text-base text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue">
+                {`${
+                  filteredList.filter((item) => !item.completed).length
+                } items left`}
+              </span>
+              <div className="flex-row hidden gap-4 mx-auto font-bold md:flex group text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue">
+                <span
+                  className={`hover:cursor-pointer hover:text-light-very-dark-grayish-blue dark:hover:text-dark-light-grayish-blue-hover ${
+                    filter.all ? "text-primary-bright-blue" : ""
+                  }`}
+                  onClick={(e) => handleFilter(e)}
+                >
+                  All
+                </span>
+                <span
+                  className={`hover:cursor-pointer hover:text-light-very-dark-grayish-blue dark:hover:text-dark-light-grayish-blue-hover ${
+                    filter.active ? "text-primary-bright-blue" : ""
+                  }`}
+                  onClick={(e) => handleFilter(e)}
+                >
+                  Active
+                </span>
+                <span
+                  className={`hover:cursor-pointer hover:text-light-very-dark-grayish-blue dark:hover:text-dark-light-grayish-blue-hover ${
+                    filter.completed ? "text-primary-bright-blue" : ""
+                  }`}
+                  onClick={(e) => handleFilter(e)}
+                >
+                  Completed
+                </span>
+              </div>
+              <div
+                className="text-sm sm:text-base text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue hover:cursor-pointer hover:text-light-very-dark-grayish-blue hover:dark:text-dark-light-grayish-blue"
+                onClick={clearCompleted}
+              >
+                Clear Completed
+              </div>
+            </div>
+          </Card>
+        </div>
+        <Card
+          className={
+            "md:hidden rounded-md shadow-2xl shadow-light-light-grayish-blue dark:shadow-dark-very-dark-blue"
+          }
+        >
+          <div className="flex flex-row justify-center gap-6 mx-auto font-bold group text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue">
             <span
               className={`hover:cursor-pointer hover:text-light-very-dark-grayish-blue dark:hover:text-dark-light-grayish-blue-hover ${
                 filter.all ? "text-primary-bright-blue" : ""
@@ -86,14 +172,11 @@ function TodoList({ todos, setTodos }) {
               Completed
             </span>
           </div>
-          <div
-            className="text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue hover:cursor-pointer hover:text-light-very-dark-grayish-blue hover:dark:text-dark-light-grayish-blue"
-            onClick={clearCompleted}
-          >
-            Clear Completed
-          </div>
-        </div>
-      </Card>
+        </Card>
+        <span className="flex items-center justify-center my-5 text-sm text-light-dark-grayish-blue dark:text-dark-dark-grayish-blue">
+          Drag and drop to reorder list
+        </span>
+      </div>
     </div>
   );
 }
