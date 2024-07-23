a9b003bd02152a535f6deee16be70b165e6ac0ba
jrlayton <jreaylayton@gmail.com>
2024-07-22 21:22:26 -0700

    Include different padding for small screen sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ className, children }) {
   return (
     <div
-      className={`group bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue w-full px-6 py-5 ${className}`}
+      className={`group bg-light-very-light-gray dark:bg-dark-very-dark-desaturated-blue w-full px-4 py-3 sm:px-6 sm:py-4 ${className}`}
     >
       {children}
     </div>
