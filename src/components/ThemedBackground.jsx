67f790f93490f74a563aa620a0e10704ae5f4313
jrlayton <jreaylayton@gmail.com>
2024-07-22 21:20:00 -0700

    Implement top offsets for mobile screen

--- src/components/ThemedBackground.jsx
+++ src/components/ThemedBackground.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ThemedBackground({ className }) {
   return (
     <div
-      className={`bg-light-very-light-gray dark:bg-dark-very-dark-blue absolute top-[300px] h-[calc(100vh-300px)] w-[100vw] ${className}`}
+      className={`bg-light-very-light-gray dark:bg-dark-very-dark-blue absolute top-[200px] sm:top-[300px] h-[calc(100vh-200px)] sm:h-[calc(100vh-300px)] w-[100vw] ${className}`}
     />
   );
 }
