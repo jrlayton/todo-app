02f9c2ff766a71c3c444cbfad2db7aee797612be
jrlayton <jreaylayton@gmail.com>
2024-07-22 21:23:49 -0700

    Include banner heights for small screen sizes

--- src/components/ThemedBanner.jsx
+++ src/components/ThemedBanner.jsx
@@ -4,10 +4,10 @@ function ThemedBanner({ className }) {
   return (
     <>
       <div
-        className={`dark:hidden bg-sm-banner-light sm:bg-lg-banner-light bg-cover bg-no-repeat absolute h-[300px] w-[100vw] ${className}`}
+        className={`dark:hidden bg-sm-banner-light sm:bg-lg-banner-light bg-cover bg-no-repeat absolute h-[200px] sm:h-[300px] w-[100vw] ${className}`}
       />
       <div
-        className={`hidden dark:block bg-sm-banner-dark sm:bg-lg-banner-dark bg-cover bg-no-repeat absolute h-[300px] w-[100vw] ${className}`}
+        className={`hidden dark:block bg-sm-banner-dark sm:bg-lg-banner-dark bg-cover bg-no-repeat absolute h-[200px] sm:h-[300px] w-[100vw] ${className}`}
       />
     </>
   );
